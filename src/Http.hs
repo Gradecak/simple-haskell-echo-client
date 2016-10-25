c8d2ea88ff151bac619fdbe94f9336862765931a
Marijan Gradecak <gradecam@tcd.ie>
2016-10-25 14:38:01 +0100

    client now fully reads response from socket before continuing

--- src/Http.hs
+++ src/Http.hs
@@ -1,11 +1,19 @@
 module Http where
 
+import Data.List.Split (splitOn)
 
 generateGetHeader :: String -> -- Remote URL eg "www.host1.com:80"
                      String -> -- Remote Resource URL "/path/file.html"
+                     String -> -- Paramater and its value
                      String    -- Returns  HTTP GET header 
-generateGetHeader host resource = "GET " ++ resource ++ " HTTP/1.1\r\n" ++ "Host: " ++ host ++ "\r\n"
+generateGetHeader host resource params =
+  "GET " ++ resource ++ "?" ++ params ++ " HTTP/1.1\r\n"
+  ++ "Host: " ++ host ++ "\r\n"
   ++ "Accept-Encoding: gzip, deflate \r\n"
   ++ "Accept: text/htmlapplication/xhtml+xml,application/xml \r\n"
-  ++ "Connection: Keep-Alive \r\n"
-  ++ "Upgrade-Insecure-Requests: 1\r\n\r\n"
+  ++ "Connection: keep-alive \r\n\r\n"
+
+-- remove the headers and return the body of a given http string 
+parseBody:: String -> -- http string
+            String    -- returned body of http string 
+parseBody res = last (splitOn "\r\n\r\n" res )
--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,45 @@
 module Main where
 
 import qualified Network.Socket as Net
-import qualified Network.Socket.ByteString as NetBs
-import qualified Data.ByteString.Char8 as ByteS
-import qualified Http (generateGetHeader)
+import Network.Socket.ByteString (sendAll, recv)
+import Data.ByteString.Char8 (unpack, pack)
+import qualified Data.ByteString as B
+import Http (generateGetHeader, parseBody)
+import System.Environment (getArgs)
 
+-- initialise a client socket
 initClient :: IO Net.Socket
 initClient = do
   let hints = Net.defaultHints {Net.addrSocketType = Net.Stream}
   addr:_ <- Net.getAddrInfo (Just hints) (Just "localhost") (Just "5000")
   sock <- Net.socket (Net.addrFamily addr) (Net.addrSocketType addr) (Net.addrProtocol addr)
   Net.setSocketOption sock Net.ReuseAddr 1
+  Net.setSocketOption sock Net.NoDelay 0
   return sock
 
-sendMessage :: IO Net.Socket -> IO()
-sendMessage iosock = do
-  let msg = Http.generateGetHeader "127.0.0.1" "/echo.php"
-      bytes = ByteS.pack msg
-      remoteAddr = Net.SockAddrInet 8000 ( Net.tupleToHostAddress (127, 0, 0, 1))
-  print $ ByteS.length bytes
+--continuously read from socket until it sends length 0 message (ie remote socket is finished sending)
+readSock :: Net.Socket -> Int -> IO [B.ByteString] -> IO B.ByteString 
+readSock _ 0 iob = fmap B.concat iob
+readSock s _ m = do
+  msg <- recv s 4096
+  readSock s (B.length msg) $ m >>= (\p -> return(p++[msg]))
+
+--send a message to remote socket and return its response
+sendMessage :: String -> String -> String -> IO Net.Socket -> IO String
+sendMessage remote port params iosock = do
+  let bytes = pack $ generateGetHeader remote "/echo.php" params
+  addr:_ <- Net.getAddrInfo Nothing (Just remote) (Just port) -- resolves hostname if hostname provided instead of ip
   sock <- iosock
-  Net.connect sock remoteAddr
-  _ <- NetBs.send sock bytes
-  response <- NetBs.recv sock 4096
+  Net.connect sock (Net.addrAddress addr)
+  sendAll sock bytes
+  response <-  readSock sock 1 (return [])
   print response
+  return $ parseBody $ unpack response
 
 main :: IO ()
 main = do
+  (remote:port:msg:_) <- getArgs
   sock <- initClient
-  sendMessage $ return sock
-  putStrLn "hello worl"
+  re <- sendMessage remote port msg $ return sock
+  putStrLn re
+  Net.close sock
